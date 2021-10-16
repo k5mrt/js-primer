b9e924ec38e4388ff240fb7216d82c4654505cb3
Keigo Murata <kei555_go@ybb.ne.jp>
2021-10-16 17:42:16 +0900

    演算子 まで学習

---
+++ 01_Basic/01_js.js
@@ -0,0 +1,14 @@
+// `name`という名前の変数を宣言
+const name = "azu";
+// `NAME`という名前の変数を宣言
+const NAME = "azu";
+
+// 式や文の間にスペースがいくつあっても同じ意味となる
+1 + 1;
+1  +  1;
+
+"use strict";
+// このコードはstrict modeで実行される
+
+"use strict";
+mistypedVariable = 42; // => ReferenceError
---
+++ 01_Basic/03_ variable.js
@@ -0,0 +1,24 @@
+//const bookTitle = "Javascript Primer";
+
+/*
+const bookTitle = "JavaScript Primer",
+      bookeCategory = "プログラミング";
+*/
+
+let bookTitle;
+bookTitle = "JavaScript Primer";
+
+let count = 0;
+count = 1;
+count = 2;
+count = 3;
+
+// TEN_NUMBER という変数は常に10という値を示す
+const TEN_NUMBER = 10;
+
+// `const`でオブジェクトを定義している
+const object = {
+  key: "値"
+};
+// オブジェクトそのものは変更できてしまう
+object.key = "新しい値";
